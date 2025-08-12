0c2c34630c9208cae4bf17f5c3d289ef5e03579c
Uleko Samuel <@samuelblessing142008@gmail.com>
2025-08-12 22:41:54 +0100

    First Commit

---
+++ backend.py
@@ -0,0 +1,15 @@
+import requests
+
+API_Key= "489176b587c21459245061f5c733de8c"
+
+def get_data(place, days = None):
+    url =f"https://api.openweathermap.org/data/2.5/forecast?q={place}&appid={API_Key}&units=metric"
+    response = requests.get(url)
+    data = response.json()
+    filter_data = data["list"]
+    nr_values = 8 * days
+    filter_data = filter_data[:nr_values]
+    return filter_data
+
+if __name__ == "__main__":
+    print(get_data(place = "lagos, days = 3"))
