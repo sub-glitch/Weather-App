2fee713c3b9d27354ae60be2ecdda35af2470946
Uleko Samuel <@samuelblessing142008@gmail.com>
2025-09-14 23:51:37 +0100

    Updated test file

---
+++ tests/test_backend.py
@@ -0,0 +1,12 @@
+from backend import get_data
+
+def test_get_data_returns_list():
+    data = get_data("Lagos", 1)
+    assert isinstance(data, list)
+
+def test_get_data_handles_invalid_city():
+    try:
+        data = get_data("NotACity", 1)
+        assert data == [] or data is None
+    except Exception:
+        assert True 
